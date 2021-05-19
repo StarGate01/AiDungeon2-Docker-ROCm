28a51eae0bdad45888d9d827f0c75ebd24bfbbac
Christoph Honal <christoph.honal@web.de>
2021-05-19 00:48:02 +0000

    Add rm flag to run

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ echo "Using groups: $GID_VIDEO, $GID_RENDER, mapping directory $MAPDIR"
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-docker run -it --net=host -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -e DISPLAY=$DISPLAY -v "$GAMEDIR:/app/game:rw" -v "$MAPDIR/models:/app/models:ro" -v "$MAPDIR/cache:/app/cache:rw" -p 5000:5000 --device=/dev/kfd --device=/dev/dri --group-add $GID_VIDEO --group-add $GID_RENDER --shm-size 8G aidungeon2-rocm:latest "$@"
+docker run --rm -it --net=host -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -e DISPLAY=$DISPLAY -v "$GAMEDIR:/app/game:rw" -v "$MAPDIR/models:/app/models:ro" -v "$MAPDIR/cache:/app/cache:rw" -p 5000:5000 --device=/dev/kfd --device=/dev/dri --group-add $GID_VIDEO --group-add $GID_RENDER --shm-size 8G aidungeon2-rocm:latest "$@"
